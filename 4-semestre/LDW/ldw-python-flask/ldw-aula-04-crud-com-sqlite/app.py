10f6369c0da585fc176f1db7734f21d4f9c3f4af
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-01 22:21:37 -0300

    feat: implementa estrutura inicial do aplicativo Flask com rotas e banco de dados

---
+++ 4-semestre/LDW/ldw-python-flask/ldw-aula-04-crud-com-sqlite/app.py
@@ -0,0 +1,32 @@
+# Importando o Flask
+from flask import Flask, render_template
+# Importando o Controller (routes.py)
+from controllers import routes
+# Importando os Models
+from models.database import db
+# Importando a biblioteca para manipulação de S.O
+import os
+
+# Criando uma instância do Flask 
+app = Flask(__name__, template_folder='views') # esse parâmetro representa o nome da aplicação
+
+routes.init_app(app)
+
+# Extraindo o diretório absoluto do arquivo
+dir = os.path.abspath(os.path.dirname(__file__))
+
+# Criando o arquivo do banco
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(dir, '/models/games.sqlite3')
+
+# se for executado diretamente pelo interpretador 
+# camada de segurança; se eu importar o app em outro arquivo, não vai rodar o servidor;
+if __name__ == '__main__':
+    # Enviando o Flask para o SQLAlchemy
+    db.init_app(app=app)
+    
+    # Verificar no início da aplicação se o BD já existe. Se não, ele cria.
+    with app.test_request_context():
+        db.create_all() 
+        
+    # iniciando o servidor
+    app.run(host="localhost", port=5000, debug=True) 
---
+++ 4-semestre/LDW/ldw-python-flask/ldw-aula-04-crud-com-sqlite/controllers/routes.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for
+import urllib # envia requisições
+import json # conversão de dados json
+
+def init_app(app):
+    # lista 
+    # a lista veio pra cá pq dentro da função ela era reiniciada a cada requisição
+    players = ['Giovana', 'Amanda', 'Igor', 'Diego']
+    gamelist = [{'Título': 'CS 1.6', 'Ano': 1996, 'Categoria': 'FPS Online'}]
+    
+    
+    # definindo a rota principal da aplicação '/'
+    @app.route('/')
+    # toda rota precisa de um função para executar 
+    def home():
+        return render_template('index.html')
+
+
+    # definindo a rota principal da aplicação '/'
+    @app.route('/games', methods=['GET', 'POST'])
+    # toda rota precisa de um função para executar 
+    def games():
+        # essas variáveis estariam vindo de fora 
+        title = 'Tarisland'
+        year = 2022
+        category = 'MMORPG'
+        # dicionário 
+        console = {'Nome' : 'PS5', 'Fabricante': 'Sony', 'Ano': 2020}
+        
+        # tratando uma requisição POST com request
+        if request.method == 'POST':
+            # coletando o texto da input 
+            # player é o nome da caixinha que eu criei no form
+            if request.form.get('player'):
+                players.append(request.form.get('player'))
+                return redirect(url_for('games'))
+        
+        # o primeiro title é a var que vai ser criada na página 
+        return render_template('games.html', title=title, year=year, category=category, players=players, console=console)
+    
+    
+    @app.route('/newGame', methods=['GET', 'POST'])
+    def newGame():
+        if request.method == 'POST':
+            if request.form.get('title') and request.form.get('year') and request.form.get('category'):
+                gamelist.append({'Título': request.form.get('title'), 'Ano': request.form.get('year'), 'Categoria' : request.form.get('category')})
+                return redirect(url_for('newGame'))
+                
+        return render_template('newGame.html', gamelist=gamelist)
+    
+    
+    @app.route('/apigames', methods=['GET', 'POST'])
+    # Criando parâmetros para a rota
+    # Para string <id>
+    # Para int <int:id>
+    @app.route('/apigames/<int:id>', methods=['GET', 'POST'])
+    def apigames(id=None): # parâmetro opcional
+        url = 'https://www.freetogame.com/api/games' # seta a url  
+        response = urllib.request.urlopen(url) # faz request abrindo a url
+        data = response.read() # le os dados
+        gameList = json.loads(data) # transforma os dados em um dicionario
+        # verificando se o parâmetro foi enviado
+        if id:
+            gameInfo = []
+            for game in gameList:
+                if game['id'] == id: # Comparando os IDs
+                    gameInfo = game
+                    break
+            if gameInfo:
+                return render_template('gameinfo.html', gameInfo=gameInfo)
+            else: return f'Game com a id {id} não foi encontrado.'
+        else: 
+            return render_template('apigames.html', gameList=gameList)
